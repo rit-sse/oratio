912ff0fae96990ac138675c68375a43b090c36a0
Cody Krieger <cody.krieger@gmail.com>
2011-03-11 12:45:23 -0500

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oratio::Application.config.secret_token = '2847d15cd926979bbbe324fcb9e09f9f1be19d42d609b3e9a6dcab4169fba41c4f877d5d9333ac6128570a5f4f00550d57b440e094b7576bb949a125ee626fb2'
