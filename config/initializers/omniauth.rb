e3f2d847204132edd46abfd54c2476ca2c8618b5
Cody Krieger <cody.krieger@gmail.com>
2011-03-22 16:35:15 -0400

    omniauth + rit ldap = win

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
-Rails.application.configure.middleware.use OmniAuth::Builder do
-  provider :ldap, :host => 'ldap.rit.edu', :port => 636, :method => :ssl, 
+require 'omniauth'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :LDAP, :host => 'ldap.rit.edu', :port => 636, :method => :ssl, 
                   :base => "ou=people,dc=rit,dc=edu", :uid => "uid"
 end
