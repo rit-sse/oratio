dbeca5c26d5b80777b2bceb94237e753d9500986
Cody Krieger <cody.krieger@gmail.com>
2011-03-22 18:24:55 -0400

    Diagnostics + flash!!!

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -3,5 +3,13 @@ class DiagnosticsController < ApplicationController
   
   def index
   end
+  
+  def test_flash_notice
+    redirect_to diagnostics_path, :notice => "Testing notice!"
+  end
+  
+  def test_flash_alert
+    redirect_to diagnostics_path, :alert => "Testing alert!"
+  end
 
 end
