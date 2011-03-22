c306fb7203f209ca39e84adf36f56d46ef866b79
Cody Krieger <cody.krieger@gmail.com>
2011-03-22 16:06:30 -0400

    more styles, lighter background texture, link styling, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def title(page_title, head_title = nil)
+    head_title = page_title if head_title.nil?
+    content_for :page_title do
+      "<h1 id='page_title'>#{page_title}</h1>".html_safe
+    end
+    content_for :head_title do
+      head_title
+    end
+  end
+  
 end
