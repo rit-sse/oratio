b7b6c3646d2e5ee84bea59d27b8bb437395062b2
Brian To <bxt5647@rit.edu>
2011-03-23 01:32:16 -0400

    Added Migration: Added Cody/Brian to AllowedUsers table :-)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110322204552) do
+ActiveRecord::Schema.define(:version => 20110323051041) do
 
   create_table "allowed_users", :force => true do |t|
     t.string   "name",       :null => false
