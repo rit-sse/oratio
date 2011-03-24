652afe322f065b228c4425206d5a5534aba047a3
Cody Krieger <cody.krieger@gmail.com>
2011-03-24 02:54:28 -0400

    added actions, clear helpers to apphelper, changed some main page listing styles, added new template button, added some form styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,22 @@
 module ApplicationHelper
   
+  def clear
+    "<div class='clear'></div>".html_safe
+  end
+  
   def title(page_title, head_title = nil)
     head_title = page_title if head_title.nil?
     content_for :page_title do
-      "<h1 id='page_title'>#{page_title}</h1>".html_safe
+      "<h1 id='page-title'>#{page_title}</h1>".html_safe
     end
     content_for :head_title do
       head_title
+    end
+  end
+  
+  def actions(&block)
+    content_for :actions do
+      with_output_buffer(&block)
     end
   end
   
