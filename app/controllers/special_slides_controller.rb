7a5bd6b7ac4d36cc3ce97d26dac13baa726da8ac
Cody Krieger <cody.krieger@gmail.com>
2011-04-21 19:43:51 -0400

    UGH, working on initial google calendar api integration, but I keep getting 401/token invalid. what gives?

--- app/controllers/special_slides_controller.rb
+++ app/controllers/special_slides_controller.rb
@@ -1,23 +1,21 @@
 class SpecialSlidesController < ApplicationController
-  require 'net/http'
+  require 'net/https'
+  require 'uri'
   
-  before_filter :check_google_login
+  before_filter :check_google_login, :except => [:login, :do_login]
 
   def index
-    @login_token = session[:google_login_auth]
+    resp = get_feed('https://www.google.com/calendar/feeds/default/private/full')
+    @resp = resp.body
   end
-
-  def login
-  end
-
+  
+  def login ; end
   def logout
     session[:google_login_auth] = nil
     redirect_to :action => :index
   end
 
   def do_login
-    http = Net::HTTP.new('www.google.com', 443)
-    http.use_ssl = true
     path = '/accounts/ClientLogin'
 
     data = "accountType=HOSTED_OR_GOOGLE&Email=#{params[:email]}" \
@@ -25,7 +23,7 @@ class SpecialSlidesController < ApplicationController
       "&service=wise"
 
     req_headers = { "Content-Type" => "application/x-www-form-urlencoded" }
-    resp, data = http.post(path, data, req_headers)
+    resp, data = google_http.post(path, data, req_headers)
 
     cl_string = data[/Auth=(.*)/, 1]
     if cl_string.nil?
@@ -39,6 +37,26 @@ class SpecialSlidesController < ApplicationController
   end
 
 protected
+  def google_http
+    return @google_http unless @google_http.nil?
+
+    http = Net::HTTP.new('www.google.com', 443)
+    http.use_ssl = true
+    @google_http = http
+  end
+
+  def get_feed(uri)
+    uri = URI.parse(uri)
+    req_headers = { "Content-Type" => "application/x-www-form-urlencoded" }
+    req_headers["Authorization"] = "GoogleLogin auth=#{session[:google_login_auth]}"
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Get.new(uri.request_uri, req_headers)
+    return http.request(req)
+  end
 
   def check_google_login
     if session[:google_login_auth].nil?
