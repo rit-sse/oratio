ba9f2048ebdb2effd5ecce92d38b9428e9e06cd4
Cody Krieger <cody.krieger@gmail.com>
2011-04-22 11:45:03 -0400

    removed clientlogin junk

--- app/controllers/special_slides_controller.rb
+++ app/controllers/special_slides_controller.rb
@@ -21,42 +21,7 @@ class SpecialSlidesController < ApplicationController
     end
   end
   
-  def login ; end
-  def logout
-    session[:google_login_auth] = nil
-    redirect_to :action => :index
-  end
-
-  def do_login
-    path = '/accounts/ClientLogin'
-
-    data = "accountType=HOSTED_OR_GOOGLE&Email=#{params[:email]}" \
-      "&Passwd=#{params[:password]}" \
-      "&service=wise"
-
-    req_headers = { "Content-Type" => "application/x-www-form-urlencoded" }
-    resp, data = google_http.post(path, data, req_headers)
-
-    cl_string = data[/Auth=(.*)/, 1]
-    if cl_string.nil?
-      flash[:alert] = "Login failed. Please try again."
-      render :login
-    else
-      flash[:notice] = "Login successful."
-      session[:google_login_auth] = cl_string
-      redirect_to :action => :index
-    end
-  end
-
 protected
-  def google_http
-    return @google_http unless @google_http.nil?
-
-    http = Net::HTTP.new('www.google.com', 443)
-    http.use_ssl = true
-    @google_http = http
-  end
-
   def get_feed(uri)
     uri = URI.parse(uri)
     req_headers = { "Content-Type" => "application/x-www-form-urlencoded" }
@@ -68,15 +33,6 @@ protected
 
     req = Net::HTTP::Get.new(uri.request_uri, req_headers)
     return http.request(req)
-  end
-
-  def check_google_login
-    if session[:google_login_auth].nil?
-      flash[:notice] = "Please log in to Google to continue."
-      redirect_to :action => :login
-    else
-      @logged_in = true
-    end
   end
 
 end
