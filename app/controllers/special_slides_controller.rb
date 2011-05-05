87fbc1c634a15ae032cef8c153b253d468cc8556
Cody Krieger <cody.krieger@gmail.com>
2011-05-05 05:20:45 -0400

    removed logic from special_slides controller

--- app/controllers/special_slides_controller.rb
+++ app/controllers/special_slides_controller.rb
@@ -1,50 +1,5 @@
 class SpecialSlidesController < ApplicationController
-  require 'net/https'
-  require 'uri'
-  require 'xmlsimple'
-
-  # before_filter :check_google_login, :except => [:login, :do_login]
-  
-  COMMON_FORMAT = '%A, %B %e %Y'
-
   def index
-    resp = get_feed('https://www.google.com/calendar/feeds/ssemember@gmail.com/public/full')
-    @resp = resp.body
-
-    @events = []
-
-    @tags = []
-    data = XmlSimple.xml_in(resp.body)
-    data['entry'].each do |item|
-      item.sort.each do |k, v|
-        # @tags += [k]
-        if ["title", "comments", "when", "where", "who"].include? k
-          if k == "title"
-            @events += [{ :title => v[0]['content'].to_s }]
-          elsif k == "when"
-            @events.last[:start_time] = DateTime.parse(v[0]['startTime'].to_s)
-            @events.last[:end_time] = DateTime.parse(v[0]['endTime'].to_s)
-          elsif k == "where"
-            @events.last[:where] = v[0]['valueString'].to_s
-          end
-        end
-      end
-    end
-  end
-
-protected
-
-  def get_feed(uri)
-    uri = URI.parse(uri)
-    req_headers = { "Content-Type" => "application/x-www-form-urlencoded" }
-    # req_headers["Authorization"] = "GoogleLogin auth=#{session[:google_login_auth]}"
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    req = Net::HTTP::Get.new(uri.request_uri, req_headers)
-    return http.request(req)
   end
 
 end
